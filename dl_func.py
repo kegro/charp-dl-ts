3c1d06b43a6c35a50af14f955ee2768cf5d58c7e
Kenneth <kegr@dhigroup.com>
2019-06-28 16:36:57 +0200

    Update dl_func.py

--- dl_func.py
+++ dl_func.py
@@ -188,8 +188,9 @@ def Archi_GRURNN(X, nbclasses):
 #-----------------------------------------------------------------------
 def trainTestModel(model, X_train, Y_train_onehot, X_test, Y_test_onehot, out_model_file, **train_params):
         #---- variables
-        n_epochs = train_params.setdefault("n_epochs", 100)
-        batch_size = train_params.setdefault("batch_size", 32)
+        #n_epochs = train_params.setdefault("n_epochs", 100)
+        n_epoch = 100
+	batch_size = train_params.setdefault("batch_size", 32)
 
         lr = train_params.setdefault("lr", 0.001)
         beta_1 = train_params.setdefault("beta_1", 0.9)
@@ -227,7 +228,8 @@ def trainTestModel(model, X_train, Y_train_onehot, X_test, Y_test_onehot, out_mo
 #-----------------------------------------------------------------------
 def trainTestValModel(model, X_train, Y_train_onehot, X_val, Y_val_onehot, X_test, Y_test_onehot, out_model_file, **train_params):
 	#---- variables
-	n_epochs = train_params.setdefault("n_epochs", 100)
+	#n_epochs = train_params.setdefault("n_epochs", 100)
+	n_epoch = 100
 	batch_size = train_params.setdefault("batch_size", 32)
 	
 	lr = train_params.setdefault("lr", 0.001)
